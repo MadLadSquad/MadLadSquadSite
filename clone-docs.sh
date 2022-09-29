7e6bcd3db7be28659dc4d80e78006f96b19e6aea
Madman10K <stanv.contact176@gmail.com>
2022-09-29 10:02:42 +0300

    Updated docs to update on schedule and have the correct links

--- clone-docs.sh
+++ clone-docs.sh
@@ -6,3 +6,6 @@ git clone https://github.com/MadLadSquad/UntitledVulkanGameEngine.wiki.git Untit
 git clone https://github.com/MadLadSquad/UntitledLog.wiki.git UntitledLog/
 git clone https://github.com/MadLadSquad/UntitledLinuxGameManager.wiki.git UntitledLinuxGameManager/
 git clone https://github.com/MadLadSquad/UntitledImGuiFramework.wiki.git UntitledImGuiFramework/
+
+find ./ -type f -exec sed -i 's/https:\/\/github.com\/MadLadSquad\//https:\/\/madladsquad.com\/docs\//g' {} \;
+find ./ -type f -exec sed -i 's/\/wiki\//\//g' {} \;
