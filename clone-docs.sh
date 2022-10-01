3e1b6d332d21d0f0e2b6d96164a1e0c51a9bdca1
Stanislav Vasilev <stanv.contact176@gmail.com>
2022-10-02 01:48:32 +0300

    Update clone-docs.sh

--- clone-docs.sh
+++ clone-docs.sh
@@ -6,7 +6,10 @@ git clone https://github.com/MadLadSquad/UntitledVulkanGameEngine.wiki.git Untit
 git clone https://github.com/MadLadSquad/UntitledLog.wiki.git UntitledLog/
 git clone https://github.com/MadLadSquad/UntitledLinuxGameManager.wiki.git UntitledLinuxGameManager/
 git clone https://github.com/MadLadSquad/UntitledImGuiFramework.wiki.git UntitledImGuiFramework/
+
 git clone https://github.com/MadLadSquad/UntitledTextGenerator.wiki.git UntitledTextGenerator/
+sed -i '1 i\{% raw %}' UntitledTextGenerator/Templating-guide.md
+echo "{% endraw %}" UntitledTextGenerator/Templating-guide.md
 
 find ./ -type f -exec sed -i 's/https:\/\/github.com\/MadLadSquad\//https:\/\/madladsquad.com\/docs\//g' {} \;
 find ./ -type f -exec sed -i 's/\/wiki\//\//g' {} \;
