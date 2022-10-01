4f8dbddb67e457c3f959c4942e3c2c00feb262ae
Stanislav Vasilev <stanv.contact176@gmail.com>
2022-10-02 02:48:46 +0300

    Update clone-docs.sh

--- clone-docs.sh
+++ clone-docs.sh
@@ -7,9 +7,10 @@ git clone https://github.com/MadLadSquad/UntitledLog.wiki.git UntitledLog/
 git clone https://github.com/MadLadSquad/UntitledLinuxGameManager.wiki.git UntitledLinuxGameManager/
 git clone https://github.com/MadLadSquad/UntitledImGuiFramework.wiki.git UntitledImGuiFramework/
 
+# Please don't touch this there are a lot of problems with this specific file
 git clone https://github.com/MadLadSquad/UntitledTextGenerator.wiki.git UntitledTextGenerator/
 sed -i '1 i\{% raw %}' UntitledTextGenerator/Templating-guide.md
-#echo "{% endraw %}" >> UntitledTextGenerator/Templating-guide.md
+echo "{% endraw %}" >> UntitledTextGenerator/Templating-guide.md
 
 find ./ -type f -exec sed -i 's/https:\/\/github.com\/MadLadSquad\//https:\/\/madladsquad.com\/docs\//g' {} \;
 find ./ -type f -exec sed -i 's/\/wiki\//\//g' {} \;
