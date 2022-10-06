7605621d8a3bcf86a9dc777dc7d36d19b83cacda
Madman10K <stanv.contact176@gmail.com>
2022-10-06 23:08:07 +0300

    Added wiki entries for the package generator

--- clone-docs.sh
+++ clone-docs.sh
@@ -5,5 +5,8 @@ cd docs
 sed -i '1 i\{% raw %}' UntitledTextGenerator/Templating-guide.md
 echo "{% endraw %}" >> UntitledTextGenerator/Templating-guide.md
 
+sed -i '1 i\{% raw %}' UntitledPackageSchemeGenerator/Creating-your-package.md
+echo "{% endraw %}" >> UntitledPackageSchemeGenerator/Creating-your-package.md
+
 find ./ -type f -exec sed -i 's/https:\/\/github.com\/MadLadSquad\//https:\/\/madladsquad.com\/docs\//g' {} \;
 find ./ -type f -exec sed -i 's/\/wiki\//\//g' {} \;
