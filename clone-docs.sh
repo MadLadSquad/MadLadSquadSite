49a7325e1a607fdbc566ea8fda66b4fb42276fb8
Stanislav Vasilev <stanv.contact176@gmail.com>
2022-11-22 22:08:58 +0200

    Update clone-docs.sh

--- clone-docs.sh
+++ clone-docs.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+add_sidebar() {
+  cd "$1"/
+  echo -e "\n---\n" >> Home.md
+  cat _Sidebar.md >> Home.md
+  cd ..
+}
+
 cd docs
 
 # Please don't touch this there are a lot of problems with this specific file
@@ -8,15 +15,9 @@ echo "{% endraw %}" >> UntitledTextGenerator/Templating-guide.md
 sed -i '1 i\{% raw %}' UntitledPackageSchemeGenerator/Creating-your-package.md
 echo "{% endraw %}" >> UntitledPackageSchemeGenerator/Creating-your-package.md
 
-cd UntitledImGuiFramework/
-echo -e "\n---\n" >> Home.md
-cat _Sidebar.md >> Home.md
-cd ../
-
-cd UntitledVulkanGameEngine/
-echo -e "\n---\n" >> Home.md
-cat _Sidebar.md >> Home.md
-cd ../
+add_sidebar UntitledImGuiFramework
+add_sidebar UntitledVulkanGameEngine
+add_sidebar UntitledTextGenerator
 
 find ./ -type f -exec sed -i 's/https:\/\/github.com\/MadLadSquad\//https:\/\/madladsquad.com\/docs\//g' {} \;
 find ./ -type f -exec sed -i 's/\/wiki\//\//g' {} \;
