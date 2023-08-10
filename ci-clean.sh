09225efa0e773a8b46faf68f927f390239afa811
Madman10K <stanv.contact176@gmail.com>
2023-08-10 21:10:11 +0300

    Update because of the stupidity of pandoc

--- ci-clean.sh
+++ ci-clean.sh
@@ -7,6 +7,6 @@ rm -rf build/ UVKBuildTool/
 
 for i in `find ./ -type f -name '*.html' -printf '%p\n'` ; do
 	sed -i 's/\.\//https:\/\/madladsquad.com\//g' "$i"
-	sed -i 's/\/index\.html//g' "$1"
-	sed -i 's/\.html//g' "$1"
+	sed -i 's/\/index\.html//g' "$i"
+	sed -i 's/\.html//g' "$i"
 done
