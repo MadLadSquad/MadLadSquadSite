7134817d333a22451130a6d2a66468ea36edb53b
Madman10K <stanv.contact176@gmail.com>
2023-08-10 21:06:50 +0300

    Update because of the stupidity of pandoc

--- ci-clean.sh
+++ ci-clean.sh
@@ -4,3 +4,9 @@ rm -rf Components/ UBTCustomFunctions/ UVBuildTool/ .github/ docs/ monthly-newsl
 mv build/*.html .
 mv build/*/ .
 rm -rf build/ UVKBuildTool/
+
+for i in `find ./ -type f -name '*.html' -printf '%p\n'` ; do
+	sed -i 's/\.\//https:\/\/madladsquad.com\//g' "$i"
+	sed -i 's/\/index\.html//g' "$1"
+	sed -i 's/\.html//g' "$1"
+done
