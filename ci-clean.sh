9198634c115030684aaf55ee49c10321fc27e6af
Stanislav Vasilev <stanv.contact176@gmail.com>
2023-08-10 19:28:49 +0300

    Update ci-clean.sh

--- ci-clean.sh
+++ ci-clean.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 rm -rf *.md
-rm -rf Components/ UBTCustomFunctions/ UVBuildTool/ .github/
+rm -rf Components/ UBTCustomFunctions/ UVBuildTool/ .github/ docs/ monthly-newsletter/ untitled-desktop/
 mv build/*.html .
 mv build/*/ .
 rm -rf build/ UVKBuildTool/
-
-
