7c40c17158007c07c837528bcd4b2e608dd921d0
Stanislav Vasilev <stanv.contact176@gmail.com>
2023-08-14 00:11:26 +0300

    Update ci-clean.sh

--- ci-clean.sh
+++ ci-clean.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 rm -rf *.md
-rm -rf Components/ UBTCustomFunctions/ UVBuildTool/ .github/ docs/ monthly-newsletter/ untitled-desktop/
+rm -rf Components/ UBTCustomFunctions/ UVKBuildTool/ .github/ docs/ monthly-newsletter/ untitled-desktop/
 mv build/*.html .
 mv build/*/ .
-rm -rf build/ UVKBuildTool/
+rm -rf build/
 
 for i in `find ./ -type f -name '*.html' -printf '%p\n'` ; do
 	sed -i 's/\.\//https:\/\/madladsquad.com\//g' "$i"
