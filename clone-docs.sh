cf0d6016779f6948dea752055cf654225cdb124c
Madman10K <stanv.contact176@gmail.com>
2023-08-29 03:15:09 +0300

    Add panel documentation

--- clone-docs.sh
+++ clone-docs.sh
@@ -32,6 +32,7 @@ add_sidebar UntitledCLIParser
 add_sidebar UntitledGameSystemManager
 add_sidebar UntitledXDGBasedir
 add_sidebar UntitledImGuiTextUtils
+add_sidebar UDFPanel
 
 find ./ -type f -exec sed -i 's/https:\/\/github.com\/MadLadSquad\//https:\/\/madladsquad.com\/docs\//g' {} \;
 find ./ -type f -exec sed -i 's/\/wiki\//\//g' {} \;
