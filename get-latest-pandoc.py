8f7c3240459d7e77e7878042a98dab6bb3c51e1b
Madman10K <stanv.contact176@gmail.com>
2024-06-10 03:12:39 +0300

    Move to new github markdown

---
+++ get-latest-pandoc.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+import requests as req
+import json
+import sys
+
+def getDeb():
+    resp = req.get("https://api.github.com/repos/jgm/pandoc/releases")
+    if resp.status_code != 200 :
+        sys.exit("Couldn't fetch the latest pandoc release!")
+    else:
+        data = json.loads(resp.content)
+        binary = req.get(data[0]["assets"][0]["url"])
+
+        if binary.status_code != 200:
+            sys.exit("Couldn't download the latest deb package!")
+        else:
+            file = open(data[0]["assets"][0]["name"], "xb")
+            file.write(binary.content)
+            file.close()
+
+            print(data[0]["assets"][0]["name"])
+
+getDeb()
