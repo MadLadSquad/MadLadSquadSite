36253387a9a143d28a8415120329f27e6509f8cb
Stanislav Vasilev <stanv.contact176@gmail.com>
2024-07-03 21:59:15 +0300

    Update index.js

--- index.js
+++ index.js
@@ -86,7 +86,7 @@ function main()
         createElement("iframe", "", "", "",     
             [ 
                 [ "src", "https://uimgui.madladsquad.com/" ],
-                [ "style", "width: 100%; height: 100%;" ],
+                [ "style", "width: 100%; height: 100%; flex-grow: 1;" ],
             ], demo);
     }
 }
